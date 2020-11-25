01e57849c01c943ad2db6a695500667822360a3e
volodymyr.opatsa <volodymyr.opatsa@akurateco.com>
2020-11-25 22:11:41 +0200

    Second part of task

--- src/test/java/com/gmail/vovanoid89/PayPalTest.java
+++ src/test/java/com/gmail/vovanoid89/PayPalTest.java
@@ -8,6 +8,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import java.util.concurrent.TimeUnit;
 
+import static com.gmail.vovanoid89.ConfProperties.*;
+
 public class PayPalTest
 {
     public static LoginPage loginPage;
@@ -18,7 +20,7 @@ public class PayPalTest
     @BeforeClass
     public static void setup()
     {
-        System.setProperty("webdriver.chrome.driver", ConfProperties.getProperty("chromeDriver"));
+        System.setProperty("webdriver.chrome.driver", getProperty("chromeDriver"));
 
         driver = new ChromeDriver();
 
@@ -29,37 +31,37 @@ public class PayPalTest
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
-        driver.get(ConfProperties.getProperty("loginPage"));
+        driver.get(getProperty("loginPage"));
     }
 
     @Test
     public void payPalTest()
     {
-        loginPage.inputLogin(ConfProperties.getProperty("login"));
+        loginPage.inputLogin(getProperty("login"));
         loginPage.clickNextButton();
 
-        loginPage.inputPassword(ConfProperties.getProperty("password"));
+        loginPage.inputPassword(getProperty("password"));
         loginPage.clickLoginButton();
 
         String userWelcome = userPage.getUserWelcome();
-        Assert.assertTrue(userWelcome.contains(ConfProperties.getProperty("userNameExpected")));
+        Assert.assertTrue(userWelcome.contains(getProperty("userNameExpected")));
 
         userPage.clickOnNewTrasaction();
 
         String generalAmount = transactionDetailsPage.getGeneralAmount();
-        Assert.assertTrue(generalAmount.contains(ConfProperties.getProperty("generalAmountExpected")));
+        Assert.assertTrue(generalAmount.contains(getProperty("generalAmountExpected")));
 
         String paidWithAmount = transactionDetailsPage.getPaidWithAmount();
-        Assert.assertEquals(ConfProperties.getProperty("paidWithAmountExpected"), paidWithAmount);
+        Assert.assertEquals(getProperty("paidWithAmountExpected"), paidWithAmount);
 
         String purchaseAmount = transactionDetailsPage.getPurchaseAmount();
-        Assert.assertEquals(ConfProperties.getProperty("purchaseAmountExpected"), purchaseAmount);
+        Assert.assertEquals(getProperty("purchaseAmountExpected"), purchaseAmount);
 
         String totalAmount = transactionDetailsPage.getTotalAmount();
-        Assert.assertEquals(ConfProperties.getProperty("totalAmountExpected"), totalAmount);
+        Assert.assertEquals(getProperty("totalAmountExpected"), totalAmount);
 
         String paidWithCardLastNum = transactionDetailsPage.getPaidWithCardLastNum();
-        Assert.assertTrue(paidWithCardLastNum.contains(ConfProperties.getProperty("paidWithCardLastNumExpected")));
+        Assert.assertTrue(paidWithCardLastNum.contains(getProperty("paidWithCardLastNumExpected")));
     }
 
     @AfterClass
